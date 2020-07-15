b2e443b6cf022f2f76a827c90123a790974c4ccf
Matheus Campos <matheus.campos14@fatec.sp.gov.br>
2020-07-15 17:05:59 -0300

    Adicionando comentários a programação

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,59 +1,62 @@
 
 function populateUFs() {
-    const ufSelect = document.querySelector("select[name=uf]")
+    const ufSelect = document.querySelector("select[name=uf]") // Selecionando o elemento select name uf do HTML
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() ) // Função Curta (Abreviada)
-    .then( states => {
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados") // Chamando a função fetch
+    .then( res => res.json() ) // Executando uma função com base na resposta da função fetch
+    .then( states => { // Executando um laço de repetição com base na resposta que teremos do then
 
         for ( const state of states ) {
             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }
+        } // Populando a constante que representa o elemento select name uf com os estados que obtemos da API
 
     } )
-}
+} // Retornando a constante ufSelect. Consequentemente no HTML, o campo será populado com todos os estados brasileiros
+populateUFs() // Executando a função populateUFs
 
-populateUFs()
 
 
 
 function getCities(event) {
-    const citySelect = document.querySelector("[name=city]")
-    const stateInput = document.querySelector("[name=state]")
+    const citySelect = document.querySelector("[name=city]") // Selecionando o elemento select name city do HTML
+    const stateInput = document.querySelector("[name=state]") // Input do estado selecionado pelo usuário
 
 
-    const ufValue = event.target.value
+    const ufValue = event.target.value // event ele diz se ouve ou não uma mudança.  .target diz onde ouve a mudança.  .value retorna o valor da mundança
 
 
-    const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedState].text
+    const indexOfSelectedState = event.target.selectedIndex // Retornando o index de onde ocorreu a mudança
+    stateInput.value = event.target.options[indexOfSelectedState].text // Retornando o valor de onde ocorreu a mudança com base no index. O .options me retornara um array com todos os elementos, usando o index (indexOfSelectedState) para acessar o valor em específico 
 
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios` // Declarando uma cosntante para armazenar a url que corresponde com o estado escolhido
 
 
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
-    citySelect.disabled = true
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>" // Limpando o campo antes preenchido com cidades. Isso foi feito para não ocorrer bugs, no caso de o usuário selecionar outro estado e esse campo apenas armazenar novos valores e não limpar os valores já contido.
+    citySelect.disabled = true // Desabilitando o campo
 
 
     fetch(url)
-    .then( res => res.json() ) // Função Curta anonima(Abreviada)
+    .then( res => res.json() ) // Retornando as cidades com base no estado escolhido pelo usuário
     .then( cities => {
 
         for ( const city of cities ) {
             citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
-        }
+        } // Armazenando dentro do elemento, todas as cidades
 
-        citySelect.disabled = false
+        citySelect.disabled = false // Habilitando o campo onde o usuário escolhe a cidade
 
     } )
 }
 
-
 document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+    .querySelector("select[name=uf]") // Selecionando o elemento select name uf no HTML
+    .addEventListener("change", getCities) // Quando ouver uma mudança no elemento selecionado acima, ele executará função getCities. Quando você for executar uma função apartir do addEventListener, passar a função em específico sem abrir e fechar parênteses. Pois, se você os fizer a função será executada no momento de compilar o código, e o intuíto é você executar a função somente se o addEventListener indentificar um evento de mudança.
 
+
+
+
+    
 
 // Itens de Coleta
 
@@ -120,8 +123,13 @@ function handleSelectedItem(even) {
 
 
 // fetch - função que busca as informações na internet a partir de um requerimento (promessa).
+
 // .then - É uma condição. Caso a função consiga trazer as informações então a programação irá seguir os comando dentro desta condição.
+
 // .catch - Condição contraria a .then. Caso a função não consiga trazer o que foi pedido, ele a programação seguirá os comandos dentro desta condição
-// .querySelector - é uma funcionalidade que pesquisa. Ele especifica um grupo especifico de informações a serem buscadas.
+
+// .querySelector - é uma funcionalidade que pesquisa. Ele especifica um grupo especifico de informações a serem buscadas noo HTML.
+
 // .innerHTML - Funcionalidade que permite escrever um HTML dentro do arquivo JavaScript.
+
 // .addEventListener - Função que fica "ouve"/"observa" se aconteceu algum evento. (Esse evento pode ser qualquer coisa, como por exemplo uma mudança)
